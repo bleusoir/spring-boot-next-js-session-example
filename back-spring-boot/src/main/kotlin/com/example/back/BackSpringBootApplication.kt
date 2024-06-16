7c58ffde7d1e8a58223d5f8978a461786fc37480
bleusoir <bleusoire@gmail.com>
2024-06-16 23:44:08 +0900

    :tada: "init commit."

---
+++ back-spring-boot/src/main/kotlin/com/example/back/BackSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackSpringBootApplication>(*args)
+}
