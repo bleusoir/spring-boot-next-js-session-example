b991bb4be948c7ed05c219eee5323a241dd10a52
bleusoir <bleusoire@gmail.com>
2024-06-17 23:31:17 +0900

    :wrench: "added security config file."

---
+++ back-spring-boot/src/main/kotlin/com/example/back/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.example.back.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): DefaultSecurityFilterChain = http
+        .csrf { it.disable() }
+        .authorizeHttpRequests {
+            it.requestMatchers("/login").permitAll().anyRequest().authenticated()
+        }
+        .logout { it.permitAll() }
+        .sessionManagement { it.maximumSessions(1).maxSessionsPreventsLogin(false) }
+        .build()
+}
